9979d5253949789d9111be0272a4719542f30a58
Sergey <Sergey@localhost.localdomain>
2022-09-15 14:07:14 +0300

    Data retrieved for all available vacancies now

--- main.py
+++ main.py
@@ -65,15 +65,25 @@ if __name__ == '__main__':
 
     languages_statistics = dict()
     for language in languages:
-        lang_params = {'text': language, 'area': 1, 'period': 30}
-        lang_vacancies = get_vacancies(user_agent, lang_params)
-        salaries = [predict_rub_salary(vacancy) for vacancy in lang_vacancies['items'] if predict_rub_salary(vacancy)]
+        params = {'text': language, 'area': 1, 'period': 30, 'per_page': 100}
+        vacancies = get_vacancies(user_agent, params)
+        all_vacancies = vacancies['items']
+        page = vacancies['page'] + 1
+
+        while page < vacancies['pages']:
+            params = {'text': language, 'area': 1, 'period': 30, 'per_page': 100, 'page': page}
+            vacancies = get_vacancies(user_agent, params)
+            all_vacancies.extend(vacancies['items'])
+            print(f'Page {page} from {vacancies["pages"]} for {language} language processed.')
+            page += 1
+
+        salaries = [predict_rub_salary(vacancy) for vacancy in all_vacancies if predict_rub_salary(vacancy)]
         if not salaries:
             average_salary = None
         else:
             average_salary = int(sum(salaries) / len(salaries))
         language_stats = {
-            "vacancies_found": lang_vacancies['found'],
+            "vacancies_found": vacancies['found'],
             "vacancies_processed": len(salaries),
             "average_salary": average_salary
         }
